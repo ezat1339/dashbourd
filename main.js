0621609d4c523f0a0ee532dfe8558e9b61684a65
ezat1339 <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>
2024-08-02 01:11:17 +0300

    update main.js

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ http.createServer(function (req, res) {
  if (err) {
   res.end('./error.html')
  }
-  fs.readFile('index.html', function(err, data) {
-    res.writeHead(404, {'Content-Type': 'text/html'});
+  fs.readFile('indx.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     res.end();
   });
