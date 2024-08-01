94cdeac7148b11a97c8a9940d3781a232801b17a
ezat1339 <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>
2024-08-02 01:02:06 +0300

    update

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
+ if (err) {
+  res.end('./error.html')
+ }
   fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(404, {'Content-Type': 'text/html'});
     res.write(data);
-    return res.end();
+    res.end();
   });
 }).listen(5000);
